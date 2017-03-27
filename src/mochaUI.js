8c53ec91c6d9d51f383745ac1a210a2834d4f18b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-03-27 16:11:21 +0200

    #1 lint fix for custom ui

--- src/mochaUI.js
+++ src/mochaUI.js
@@ -1,29 +1,35 @@
 import Mocha from 'mocha';
 import Suite from 'mocha/lib/suite';
 import Test from 'mocha/lib/test';
+import createCommonSuite from 'mocha/lib/interfaces/common';
 
-const mochaUI = function mochaUI(suite) {
+const mochaUI = (suite) => {
     const suites = [suite];
-    suite.on('pre-require', function(context, file, mocha) {
-        const common = require('mocha/lib/interfaces/common')(suite, context);
+    suite.on('pre-require', (context, file, mocha) => {
+        const common = createCommonSuite(suite, context);
 
+        // eslint-disable-next-line no-param-reassign
         context.run = mocha.options.delay && common.runWithSuite(suite);
-        context.feature = function(title, fn) {
-            const suite = Suite.create(suites[0], title);
-            suite.file = file;
-            suites.unshift(suite);
-            fn.call(suite);
+
+        // eslint-disable-next-line no-param-reassign
+        context.feature = (title, fn) => {
+            const feature = Suite.create(suites[0], title);
+            feature.file = file;
+            suites.unshift(feature);
+            fn.call(feature);
             suites.shift();
 
-            return suite;
+            return feature;
         };
 
+        // eslint-disable-next-line no-param-reassign
         context.scenario = context.feature;
-        context.it = function(title, fn) {
-            const suite = suites[0];
+
+        // eslint-disable-next-line no-param-reassign
+        context.it = (title, fn) => {
             const test = new Test(title, fn);
             test.file = file;
-            suite.addTest(test);
+            suites[0].addTest(test);
             return test;
         };
     });
